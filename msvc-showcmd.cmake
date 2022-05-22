58e56fd6cdd78f6281738c493366fee29200f70c
unknown <jon.smit@gmail.com>
2022-05-22 18:50:30 +0300

    update/fix

--- msvc-showcmd.cmake
+++ msvc-showcmd.cmake
@@ -1,13 +1,16 @@
 
-#https://stackoverflow.com/questions/57358039/how-to-see-the-underlying-compiler-linker-command-line-with-cmake-nmake
+# solution to show full command and disable names collision while compilation
 
-#Run cmd with admin privileges:
-#fsutil behavior set disable8dot3 1 OR  fsutil behavior set disable8dot3 3
-#(1 = all volumes / 3 = all but system colume)  - then reboot.
+# https://stackoverflow.com/questions/57358039/how-to-see-the-underlying-compiler-linker-command-line-with-cmake-nmake
 
-#Run cmd again with admin credentials and type:
-#fsutil 8dot3name strip /f /s C:
-#Repeat strip for every drive/partition.
+	# another solution to disable 8.3 names generation while compilation
+	# http://tips.defun.work/windows-perf.html
+	#Run cmd with admin privileges:
+	#fsutil behavior set disable8dot3 1 OR  fsutil behavior set disable8dot3 3
+	#(1 = all volumes / 3 = all but system colume)  - then reboot.
+	#Run cmd again with admin credentials and type:
+	#fsutil 8dot3name strip /f /s C:
+	#Repeat strip for every drive/partition.
 
 # append after project()
 
