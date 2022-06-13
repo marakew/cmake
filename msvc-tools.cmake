cbc806196b4f6ba9b927647f50d8a0ec9b6798e4
unknown <jon.smit@gmail.com>
2022-06-13 10:49:25 +0300

    update/fix

--- msvc-tools.cmake
+++ msvc-tools.cmake
@@ -178,31 +178,31 @@ set(_GS "/GS-") # Buffer Security Check - disable
 set(_FLAGS_CXX "${_GR} /EHsc")
 set(_FLAGS_C "")
 #set(_W3 " /W3")
-set(_W3
-      /W4 # Baseline reasonable warnings
-      /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
-      /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
-      /w14263 # 'function': member function does not override any base class virtual member function
-      /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
-              # be destructed correctly
-      /w14287 # 'operator': unsigned/negative constant mismatch
-      /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
-              # the for-loop scope
-      /w14296 # 'operator': expression is always 'boolean_value'
-      /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
-      /w14545 # expression before comma evaluates to a function which is missing an argument list
-      /w14546 # function call before comma missing argument list
-      /w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
-      /w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
-      /w14555 # expression has no effect; expected expression with side- effect
-      /w14619 # pragma warning: there is no warning number 'number'
-      /w14640 # Enable warning on thread un-safe static member initialization
-      /w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
-      /w14905 # wide string literal cast to 'LPSTR'
-      /w14906 # string literal cast to 'LPWSTR'
-      /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
-      /permissive- # standards conformance mode for MSVC compiler.
-  )
+set(_W3 "")
+string(APPEND _W3 " /W4") # Baseline reasonable warnings
+string(APPEND _W3 " /w14242") # 'identifier': conversion from 'type1' to 'type1', possible loss of data
+string(APPEND _W3 " /w14254") # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
+string(APPEND _W3 " /w14263") # 'function': member function does not override any base class virtual member function
+string(APPEND _W3 " /w14265") # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
+				# be destructed correctly
+string(APPEND _W3 " /w14287") # 'operator': unsigned/negative constant mismatch
+string(APPEND _W3 " /we4289") # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
+				# the for-loop scope
+string(APPEND _W3 " /w14296") # 'operator': expression is always 'boolean_value'
+string(APPEND _W3 " /w14311") # 'variable': pointer truncation from 'type1' to 'type2'
+string(APPEND _W3 " /w14545") # expression before comma evaluates to a function which is missing an argument list
+string(APPEND _W3 " /w14546") # function call before comma missing argument list
+string(APPEND _W3 " /w14547") # 'operator': operator before comma has no effect; expected operator with side-effect
+string(APPEND _W3 " /w14549") # 'operator': operator before comma has no effect; did you intend 'operator'?
+string(APPEND _W3 " /w14555") # expression has no effect; expected expression with side- effect
+string(APPEND _W3 " /w14619") # pragma warning: there is no warning number 'number'
+string(APPEND _W3 " /w14640") # Enable warning on thread un-safe static member initialization
+string(APPEND _W3 " /w14826") # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
+string(APPEND _W3 " /w14905") # wide string literal cast to 'LPSTR'
+string(APPEND _W3 " /w14906") # string literal cast to 'LPWSTR'
+string(APPEND _W3 " /w14928") # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+string(APPEND _W3 " /permissive-") # standards conformance mode for MSVC compiler.
+
 if(DRIVER)
   # specifies the __stdcall calling convention for all functions except C++ member functions
   string(APPEND _FLAGS_CXX " /Gz /KERNEL") #/Zc:threadSafeInit- 
