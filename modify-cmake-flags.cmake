0eba02b3f83ed6214934a7971f8bfb91e671ab2e
unknown <jon.smit@gmail.com>
2023-03-12 04:12:56 +0200

    update/fix

--- modify-cmake-flags.cmake
+++ modify-cmake-flags.cmake
@@ -1,4 +1,15 @@
 
+# example of usage
+
+#cmake_minimum_required(VERSION 3.22)
+
+#set(CMAKE_TOOLCHAIN_FILE L:/git_my/cmake/msvc-tools.cmake)
+
+#project(test)
+# >>>
+
+#include(L:/git_my/cmake/msvc-showcmd.cmake)
+
 foreach(l IN ITEMS C CXX)
   foreach(c IN ITEMS DEBUG MINSIZEREL RELEASE RELWITHDEBINFO)
 #     message(STATUS "${CMAKE_${l}_FLAGS_${c}}")
@@ -6,3 +17,6 @@ foreach(l IN ITEMS C CXX)
 #     message(STATUS "${CMAKE_${l}_FLAGS_${c}}")
   endforeach()
 endforeach()
+
+# <<<
+#set(CMAKE_CXX_STANDARD 20)
